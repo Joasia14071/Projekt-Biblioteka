005be395fa525c7bf6bfa84f65892f19e74055ab
Joasia14071 <joasia14071@gmail.com>
2015-02-10 20:15:06 +0100

    Revert "Revert "drugi commit""
    This reverts commit ae5c3199f66b485dd8008cba9818a1874433decc.

---
+++ library/biblioteka.cpp
@@ -0,0 +1,74 @@
+#include "biblioteka.h"
+
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+float kolejka :: getwskaznik_skalujacy1()
+{
+    return 0.2;
+}
+float kolejka :: getwskaznik_skalujacy2()
+{
+    return 0.8;
+}
+
+float kolejka :: long_kolejka()
+{
+    srand( time( NULL ) );
+    return ((rand() % 100 ) + 0); // 100 liczb_w_przedziale + startujemy od 0, losujemy dlugosc kolejki
+}
+
+void Lib :: wczytaj()
+{
+	plik.open("lektury.txt");
+
+	if( plik.good() == true )
+    {
+        cout << "Wczytano baze lektur" <<endl<<endl;
+    }
+    else cout << "Brak dostepu do bazy lektur" <<endl<<endl;
+}
+
+void Lib :: przypisz()
+{
+	int i = 1;
+	int k = 0;
+	string a;
+	while(!plik.eof())
+    {
+		plik >> a;
+		regal[i].push_back(a);
+		i=(i+1)%3;
+	}
+}
+
+void Lib :: wypisz()
+ {
+	int i = 0;
+	int k = 0;
+
+	while(k<regal[i].size())
+    {
+		cout << setw(20) << regal[i].at(k);
+		i++;
+		if (i==3)
+		 {
+			i=0;
+			k++;
+			cout << endl;
+		}
+	}
+}
+
+void Lib :: zamknij()
+{
+	plik.close();
+}
